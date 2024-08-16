f3f92b93780e3224c94c37ad4966e80186225ecc
Nguyen Quoc Anh Tuan <21130601@st.hcmuaf.edu.vn>
2024-08-16 19:22:21 +0700

    add testing class

--- kafka-consumer-example/src/test/java/com/nlu/app/KafkaConsumerExampleApplicationTests.java
+++ kafka-consumer-example/src/test/java/com/nlu/app/KafkaConsumerExampleApplicationTests.java
@@ -1,13 +1,44 @@
 package com.nlu.app;
 
+import com.nlu.app.dto.kafka.PayTMStatusDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
+@Slf4j
 class KafkaConsumerExampleApplicationTests {
+    @Container
+    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
 
-    @Test
-    void contextLoads() {
+    @DynamicPropertySource
+    public static void initKafkaProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
     }
 
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Autowired
+    public void setKafkaTemplate(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Test
+    public void testConsume() {
+        log.info("Waiting for Kafka message...");
+        var request = PayTMStatusDTO.builder()
+                        .isPay(true).transactionID("sadsadas")
+                        .username("nqat0919").build();
+        kafkaTemplate.send("paytm-topic-1", request);
+        log.info("method executed");
+    }
 }
--- kafka-demo-springboot/src/test/java/com/nlu/app/KafkaDemoSpringbootApplicationTests.java
+++ kafka-demo-springboot/src/test/java/com/nlu/app/KafkaDemoSpringbootApplicationTests.java
@@ -1,13 +1,49 @@
 package com.nlu.app;
 
+import com.nlu.app.dto.request.PaymentCreationRequest;
+import com.nlu.app.service.KafkaMessagePublisher;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+import java.time.Duration;
+
+import static org.awaitility.Awaitility.await;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 class KafkaDemoSpringbootApplicationTests {
+	@Container
+	static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
 
-	@Test
-	void contextLoads() {
+	@DynamicPropertySource
+	public static void initKafkaProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
 	}
 
+	private KafkaMessagePublisher publisher;
+
+	@Autowired
+	public void setPublisher(KafkaMessagePublisher publisher) {
+		this.publisher = publisher;
+	}
+
+	@Test
+	public void testSendEventToTopic() {
+		var request = PaymentCreationRequest.builder()
+						.isPay(true).username("test-user")
+						.build();
+		publisher.createPayment(request);
+		await().pollInterval(Duration.ofSeconds(3))
+				.atMost(Duration.ofSeconds(10))
+				.untilAsserted(() -> {
+					// assert message sent successfully to Kafka topic
+				});
+	}
 }
